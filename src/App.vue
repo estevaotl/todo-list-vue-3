651eff4cbe6c0105a64ad05825608a0704baa4ef
Estevao Leite <estevaotlnf@gmail.com>
2025-09-01 20:39:31 -0300

    Feat: implementa vuex para renderizaração das tarefas dinamicamente

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="px-3 py-10 md:px-10">
     <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
-      <TodoSpinner />
+      <TodoSpinner v-if="loading == true"/>
 
-      <TodoFormAdd />
+      <template v-else>
+        <TodoFormAdd />
 
-      <TodoItens />
+        <TodoItens />
 
-      <TodoEmpty />
+        <TodoEmpty />
+      </template>
     </div>
   </div>
 </template>
@@ -29,13 +31,16 @@ export default {
   },
   data() {
     return {
-      todos: []
+      loading: false
     }
   },
   created() {
+    this.loading = true;
     axios.get('http://localhost:3000/todos').then((response) => {
-      this.todos = response.data;
-    })
+      this.$store.commit('storeTodos', response.data);
+    }).finally(() => {
+      this.loading = false;
+    });
   }
 }
 </script>
